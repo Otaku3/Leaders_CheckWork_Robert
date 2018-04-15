2fe978b291495fd79f224257f5a6322bda650861
Otaku3 <Otakumi@m.cs.osakafu-u.ac.jp>
2018-04-15 15:50:28 +0900

    背景入れた

--- Leaders_CheckWork_Robert/ViewController.swift
+++ Leaders_CheckWork_Robert/ViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var Heroname:UILabel!
+    
+    var nameArray = ["アイアンマン","キャプテン・アメリカ","マイティ・ソー","スパイダーマン"]
+    var tmp:Int = 0
+    
+    @IBAction func pushNext(){
+        if tmp == 3{ tmp = 0}
+        else{tmp += 1}
+        Heroname.text = String(nameArray[tmp])
+    
+    }
+    
+    @IBAction func pushBack(){
+        if tmp == 0{ tmp = 3}
+        else{tmp -= 1}
+        Heroname.text = String(nameArray[tmp])
+    
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Heroname.text = String(nameArray[0])
         // Do any additional setup after loading the view, typically from a nib.
     }
 
